16b038fef361b8b47167fb2d8f3b58c3a181eb64
PedroCaaveiro <acaaveir@yahoo.es>
2025-07-27 16:32:46 +0200

    CAMBIOS

--- includes/app.php
+++ includes/app.php
@@ -1,31 +1,21 @@
 <?php 
 
 $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+
 $isLocalhost = ($host === 'localhost' || $host === '127.0.0.1' || strpos($host, 'localhost') === 0 || strpos($host, '127.0.0.1') === 0);
-
-// depurar
-//echo 'Localhost: ' . ($isLocalhost ? 'si' : 'no') . '<br>';
-
-// Definir la URL base correctamente para el entorno local
-$isLocalhost = $_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1';
 
 // URL base para rutas (controlador)
 $base = $isLocalhost
     ? 'http://localhost/Workscape/public/index.php/'
-    : 'http://workspace.infinityfree.me/';
+    : 'http://workspace.infinityfree.me/public/index.php/';
 
 // URL base para recursos estáticos (CSS, JS, imágenes)
 $baseAssets = $isLocalhost
     ? 'http://localhost/Workscape/public/'
-    : 'https://midominio.com/';
+    : 'http://workspace.infinityfree.me/public/';
 
 define('BASE_URL', $base);
 define('ASSETS_URL', $baseAssets);
-
-
-
-
-
 
 require 'funciones.php';
 require 'database.php';
