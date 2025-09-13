80053fe05be3edd38a3fcea25d5acb69e1c4f5cb
PedroCaaveiro <acaaveir@yahoo.es>
2025-09-13 13:30:58 +0200

    cambios

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 //$db = mysqli_connect('sql313.infinityfree.com', 'if0_38503538', 'Caaveiro2025', 'if0_38503538_Workspace');
 
-=======
->>>>>>> b5724c58a4c3b3e21ac7cd8cdf027fea87f8971e
 $db = mysqli_connect('localhost', 'root', 'root', 'workscape');
 
 if (!$db) {
